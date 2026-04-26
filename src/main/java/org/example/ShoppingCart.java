8953e32df7cebd5176b5f19d29c8397534c43f5c
Mac71805 <miguel71805@gmail.com>
2026-04-26 19:15:07 -0400

    Finish the Shopping Cart Class

--- src/main/java/org/example/ShoppingCart.java
+++ src/main/java/org/example/ShoppingCart.java
@@ -1,4 +1,38 @@
 package org.example;
 
+import java.util.*;
+
 public class ShoppingCart {
+
+    public List<Product> cart = new ArrayList<>();
+
+    public void addToCart(Product product) {
+        cart.add(product);
+        System.out.println("Added to cart!");
+    }
+
+    public void removeFromCart(Product product) {
+        cart.remove(product);
+        System.out.println("Removed from cart!");
+    }
+
+    public double getCartTotal() {
+        double total = 0;
+        for (Product p : cart) {
+            total += p.getPrice();
+        }
+        return total;
+    }
+
+    public void displayCart() {
+        if (cart.isEmpty()) {
+            System.out.println("Cart is empty.");
+            return;
+        }
+
+        for (Product p : cart) {
+            System.out.println(p);
+        }
+        System.out.println("Total: $" + getCartTotal());
+    }
 }
